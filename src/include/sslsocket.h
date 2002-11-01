340e69cd94b99d00c7e50d5771e15977d5474049
tellini <wiz78@users.noreply.github.com>
2002-11-01 19:01:01 +0000

    - configure: --with-openssl with no arguments enables SSL, but   doesn't modify the includes and libs paths, in order to avoid   annoying "changed system includes path order" warnings from gcc. - SSL: moved SSL_CTX from SSLSocket into its own class, so one   context can be shared by different sockets. - mod_ftp: implemented TLS login for the server side. Tried that   with proftpd + patch and it worked :) ATM, only the control   connection is secured.

--- src/include/sslsocket.h
+++ src/include/sslsocket.h
@@ -1,7 +1,7 @@
 /***************************************************************************
                                 sslsocket.h
                              -------------------
-    revision             : $Id: sslsocket.h,v 1.2 2002-10-31 19:04:14 tellini Exp $
+    revision             : $Id: sslsocket.h,v 1.3 2002-11-01 19:01:00 tellini Exp $
     copyright            : (C) 2002 by Simone Tellini
     email                : tellini@users.sourceforge.net
 
@@ -26,21 +26,27 @@
 
 #include "tcpsocket.h"
 #include "bitfield.h"
+#include "sslctx.h"
+#include "buffer.h"
 
 class SSLSocket : public TcpSocket
 {
 public:
-	typedef enum { CLIENT, SERVER } SocketType;
-
-					SSLSocket();
-					SSLSocket( int fd, bool dontclose = true );
-					SSLSocket( TcpSocket *sock, bool dontclose = true );
+					SSLSocket( SSLCtx *ctx );
+					SSLSocket( SSLCtx *ctx, int fd, bool dontclose = true );
+					SSLSocket( SSLCtx *ctx, TcpSocket *sock, bool dontclose = true );
 					~SSLSocket();
 
-	bool			SSLInitSession( SocketType type );
+	bool			SSLInitSession( SSLCtx::SessionType type );
 	void			SSLEndSession( void );
 
-	// flags is ignored
+	X509			*SSLGetPeerCert( void ) { return( SSL_get_peer_certificate( Ssl )); }
+	STACK_OF(X509)	*SSLGetPeerCertChain( void ) { return( SSL_get_peer_cert_chain( Ssl )); }
+
+	void			SSLSetCert( X509 *cert ) { SSL_use_certificate( Ssl, cert ); }
+
+	// flags is ignored unless it's MSG_PEEK, which is emulated
+	// buffering data
 	virtual bool	Send( const void *data, int size, int flags = 0 );
 	// flags is ignored, timeout too at the moment
 	virtual int		Recv( void *buffer, int size, int flags = 0, int timeout = -1 );
@@ -48,12 +54,10 @@ public:
 private:
 	BitField		SSLFlags;
 	SSL				*Ssl;
-	SSL_CTX			*SslCtx;
+	SSLCtx			*SslCtx;
+	Buffer			TmpData;
 
 	void			Setup( void );
-
-	SSL_CTX			*InitClient( void );
-	SSL_CTX			*InitServer( void );
 };
 
 #define SSLF_DONT_CLOSE			(1 << 0)
