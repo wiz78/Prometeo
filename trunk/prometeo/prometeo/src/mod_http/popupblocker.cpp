f50afa29296655ebf67389a6448ac51a2e485a41
tellini <tellini@a3000225-3594-40d3-85e5-f462ba722290>
2003-04-08 17:44:41 +0000

    Made popupblocker a bit smarter

--- trunk/prometeo/prometeo/src/mod_http/popupblocker.cpp
+++ trunk/prometeo/prometeo/src/mod_http/popupblocker.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
                               popupblocker.cpp
                              -------------------
-    revision             : $Id: popupblocker.cpp,v 1.2 2003-04-07 13:44:30 tellini Exp $
+    revision             : $Id: popupblocker.cpp,v 1.3 2003-04-08 17:44:41 tellini Exp $
     copyright            : (C) 2003 by Simone Tellini
     email                : tellini@users.sourceforge.net
 
@@ -22,22 +22,26 @@
 //---------------------------------------------------------------------------
 bool PopupBlocker::Process( void )
 {
-	int head_pre, open_pre;
+	int head_pre, open_pre, html_pre;
 
 	head_pre = Replace( "<head>",
 						"<head>\n"
 						"<script language=\"JavaScript\"><!--\n"
+						"    var _prometeo_loaded = false;\n"
 						"    function _prometeo_window_open()\n"
 						"    {\n"
-						"        if(( event.type == \"click\" ) ||\n"
-						"           ( event.type == \"dblclick\" ) ||\n"
-						"           ( event.type == \"mousedown\" ) ||\n"
-						"           ( event.type == \"mouseup\" ) ||\n"
-						"           ( event.type == \"keyup\" ) ||\n"
-						"           ( event.type == \"keydown\" ) ||\n"
-						"           ( event.type == \"keypress\" ) ||\n"
-						"           ( event.type == \"error\" ) ||\n"
-						"           ( event.type == \"submit\" )) {\n"
+						"        do_it = ( event == null ) && _prometeo_loaded;\n\n"
+						"        if( !do_it && ( event != null ))\n"
+						"           do_it = ( event.type == \"click\" ) ||\n"
+						"                   ( event.type == \"dblclick\" ) ||\n"
+						"                   ( event.type == \"mousedown\" ) ||\n"
+						"                   ( event.type == \"mouseup\" ) ||\n"
+						"                   ( event.type == \"keyup\" ) ||\n"
+						"                   ( event.type == \"keydown\" ) ||\n"
+						"                   ( event.type == \"keypress\" ) ||\n"
+						"                   ( event.type == \"error\" ) ||\n"
+						"                   ( event.type == \"submit\" );\n\n"
+						"        if( do_it ) {\n"
 						"               func = \"window.\" + \"open(\";\n"
 						"               for( i = 0; i < arguments.length; i++ ) {\n"
 						"                  if( i > 0 )\n"
@@ -53,6 +57,11 @@ bool PopupBlocker::Process( void )
 						"// --></script>\n" );
 
 	open_pre = Replace( "window.open(", "_prometeo_window_open(" );
+	
+	html_pre = Replace( "</html>",
+						"<script language=\"JavaScript\"><!--\n"
+						" _prometeo_loaded = true;\n"
+						"//--></script></html>" );
 
 	if(( head_pre >= 0 ) && ( open_pre >= 0 ))
 		Ready = ( head_pre < open_pre ) ? head_pre : open_pre;
@@ -62,6 +71,9 @@ bool PopupBlocker::Process( void )
 		Ready = open_pre;
 	else
 		Ready = Data.GetSize();
+
+	if(( html_pre >= 0 ) && ( html_pre < Ready ))
+		Ready = html_pre;
 		
 	return( true );
 }
